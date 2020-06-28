f58bac9c9f59d0cc908a41160dcc8e34dfe7f6ad
Eleanorecheng <Vexille0831@gmail.com>
2020-06-28 19:37:50 +0800

    week2 finished

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
+	var reverseAns = ''
+	for(var i=str.length-1; i >=0; i--){
+		reverseAns += str.charAt(i)
+  }
+  return reverseAns
   
 }
 
-reverse('hello');
+console.log(reverse('hello'));
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,12 @@
+//給定一個數字 n，因數就是所有小於等於 n 又可以被 n 整除的數，
+//所以最明顯的例子就是 1 跟 n，這兩個數一定是 n 的因數。現在請寫出一個函式來「印出」所有的因數
+
 function printFactor(n) {
-  
+  for (var i=0; i<=n; i++){
+	  if (n % i === 0){
+		  console.log(i)
+	  }
+  }
 }
 
-printFactor(10);
+printFactor(100);
